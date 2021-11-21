6559c3fe1222c340f83ce80fd701a185e6076325
taggeddev <denis.mikov.06@gmail.com>
2021-11-21 11:50:09 +0300

    add: Uploaded controllers folder

---
+++ Controllers/IntroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lingva.Controllers
+{
+    public class IntroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static web4.SeedData;
+
+namespace web4.Controllers
+{
+    [Route("test")]
+    public class TestController : Controller 
+    {
+        [Authorize(Roles = RoleNames.Administrator)]
+        [Route("rest")]
+        public IActionResult GetAdm() => Ok("Admin only page");
+
+        [Route("work")]
+        [Authorize(Roles = RoleNames.Moderator)]
+        public IActionResult GetMod() => Ok("Moderator only page");
+    }
+}
