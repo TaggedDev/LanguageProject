9f88b1217ada62e11dd39cf405df3ba5e080e617
TaggedDev <denis.mikov.06@gmail.com>
2021-11-25 13:53:45 +0300

    style(viewmodel): added spacing

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Lingva.ViewModels
 {
@@ -12,12 +7,15 @@ namespace Lingva.ViewModels
         [Required]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
         [Required]
+        [DataType(DataType.Password)]
         [Display(Name = "Пароль")]
         public string Password { get; set; }
+
         [Display(Name = "Запомнить?")]
         public bool RememberMe { get; set; }
-        public string ReturnURL { get; set; }
 
+        public string ReturnUrl { get; set; }
     }
 }
