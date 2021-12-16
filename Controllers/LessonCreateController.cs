119ea271d88a9baf0bf6bca6e961b748850779ae
TaggedDev <denis.mikov.06@gmail.com>
2021-12-16 15:16:48 +0300

    feat(mvc): Started work on connecting lesson create pages

--- Controllers/LessonCreateController.cs
+++ Controllers/LessonCreateController.cs
@@ -10,40 +10,54 @@ namespace Lingva.Controllers
             return View();
         }
 
-        [HttpPost]
-        public IActionResult ChooseLanguage(ChooseLanguage model)
+        [HttpPost("ChooseLanguage")]
+        public IActionResult ChooseLanguage(Lesson model)
         {
             // Когда пользователь нажимает на кнопку, он попадает сюда. В model содержится данные, 
             // взятые в radiobutton'ах
-            return View(model);
+            return RedirectToAction("ChooseTheme", model);
         }
 
         public IActionResult ChooseLanguage()
         {
-
             return View();
         }
 
-        public IActionResult LessonTemplate()   
+        [HttpPost("ChooseTheme")]
+        public IActionResult ChooseThemePost(Lesson model)
         {
-            ChooseLanguage choose = new ChooseLanguage();
-            return View(choose);
+            // Вызывается при отправке формы
+            return RedirectToAction("ChooseTask", model);
         }
 
-        public IActionResult ChooseTheme()
+        public IActionResult ChooseTheme(Lesson model)
+        {
+            // Вызывается из другого Action
+            return View(model);
+        }
+
+        [HttpPost("ChooseTask")]
+        public IActionResult ChooseTaskPost(Lesson model)
+        {
+            return RedirectToAction("ChooseTaskType", model);
+        }
+
+        public IActionResult ChooseTask(Lesson model)
+        {
+            return View(model);
+        }
+        
+        [HttpPost("ChooseTask")]
+        public IActionResult ChooseTaskTypePost(Lesson model)
         {
             return View();
         }
 
-        public IActionResult ChooseTaskType()
+        public IActionResult ChooseTaskType(Lesson model)
         {
-            return View();
+            return RedirectToAction("ChooseTaskType", model);
         }
 
-        public IActionResult ChooseTask()
-        {
-            return View();
-        }
         public IActionResult ChooseNeedTheme()
         {
             return View();
--- ViewModels/Lesson.cs
+++ ViewModels/Lesson.cs
@@ -1,7 +1,8 @@
 ﻿namespace Lingva.ViewModels
 {
-    public class ChooseLanguage
+    public class Lesson
     {
         public string Language { get; set; }
+        public ulong ThemeId { get; set; }
     }
 }
