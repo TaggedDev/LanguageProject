c7422597ab43bf487e95bfb8f8858cc24c25cd3a
taggeddev <denis.mikov.06@gmail.com>
2021-12-24 15:05:34 +0300

    feat(mvc): added profile controller and profile page

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lingva.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
