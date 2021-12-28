586ed47e9a8519f998611660ad870beb7530b5df
taggeddev <denis.mikov.06@gmail.com>
2021-12-28 11:50:12 +0300

    style(controller): Renamed Task.cs field name due class field renaming

--- Controllers/LanguageController.cs
+++ Controllers/LanguageController.cs
@@ -1,38 +1,33 @@
 ﻿using Lingva.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Lingva.Controllers
 {
     public class LanguageController : Controller
     {
-        
         public IActionResult English()
         {
-            Lesson lesson = new Lesson()
+            Task lesson = new Task()
             {
-                LessonID = 1
+                LessonParentID = 1
             };               
             return View(lesson);
         }
 
         public IActionResult Russian()
         {
-            Lesson lesson = new Lesson()
+            Task lesson = new Task()
             {
-                LessonID = 1
+                LessonParentID = 1
             };
             return View(lesson);
         }
 
         public IActionResult Deutch()
         {
-            Lesson lesson = new Lesson()
+            Task lesson = new Task()
             {
-                LessonID = 1
+                LessonParentID = 1
             };
             return View(lesson);
         }
